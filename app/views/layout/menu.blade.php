599847da14860efa8ffe2bdb40d538ce71e15ba6
Estuardo Chuy <estchuy@hotmail.com>
2016-05-11 17:21:01 -0600

    change UI

---
+++ app/views/layout/menu.blade.php
@@ -0,0 +1,62 @@
+<div class="navbar-custom">
+    <div class="container">
+        <div id="navigation">
+            <!-- Navigation Menu-->
+            <ul class="navigation-menu">
+                <li class="">
+                    <a href="/clients" class="waves-effect waves-light" onclick="javascript:$('#myPleaseWait').modal('show');"><i class="md icon-people "></i> <span> Clientes </span> </a>   
+                </li>
+                <li>
+                    <a href="/calculoCuotas" class="waves-effect waves-light" onclick="javascript:$('#myPleaseWait').modal('show');"><i class="fa fa-calculator "></i> <span> Calculo de Cuotas </span> </a>
+                </li>
+                <li class="has-submenu">
+                    <a href="#" class="waves-effect waves-light"><i class="md icon-notebook"></i> <span> Reportes </span> </a>
+                    <ul class="submenu">
+                        <li><a href="/reporte/consolidado" onclick="javascript:$('#myPleaseWait').modal('show');">Consolidado</a></li>
+                        <li><a href="/reporte/detallado" onclick="javascript:$('#myPleaseWait').modal('show');"> Detallado</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="/loan/historico" class="waves-effect waves-light" onclick="javascript:$('#myPleaseWait').modal('show');"><i class="fa fa-history "></i> <span> Historico </span> </a>
+                </li>
+            </ul>
+            <!-- End navigation menu        -->
+        </div>
+    </div>
+</div>
+    <!-- End Navigation Bar           
+
+
+
+        
+            <div class="left side-menu">
+                <div class="sidebar-inner slimscrollleft">
+                    
+                    <div id="sidebar-menu">
+                        <ul>
+
+                          <li class="text-muted menu-title">menu</li>
+
+                            <li>
+                                <a href="/clients" class="waves-effect waves-light" onclick="javascript:$('#myPleaseWait').modal('show');"><i class="md icon-people "></i> <span> Clientes </span> </a>
+                            </li>
+                            <li>
+                                <a href="/calculoCuotas" class="waves-effect waves-light" onclick="javascript:$('#myPleaseWait').modal('show');"><i class="fa fa-calculator "></i> <span> Calculo de Cuotas </span> </a>
+                            </li>
+                            <li class="has_sub">
+                                <a href="#" class="waves-effect waves-light"><i class="md icon-notebook"></i> <span> Reportes </span> </a>
+                                <ul class="list-unstyled">
+                                    <li><a href="/reporte/consolidado" onclick="javascript:$('#myPleaseWait').modal('show');">Consolidado</a></li>
+                                    <li><a href="/reporte/detallado" onclick="javascript:$('#myPleaseWait').modal('show');"> Detallado</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="/loan/historico" class="waves-effect waves-light" onclick="javascript:$('#myPleaseWait').modal('show');"><i class="fa fa-history "></i> <span> Historico </span> </a>
+                            </li>
+                        </ul>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+            <!-- Left Sidebar End -->
